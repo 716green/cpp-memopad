3e4dbf597809d25a031f3d301da43f9632978816
Bob Bass <bob@bbass.co>
2021-03-03 11:01:03 -0500

    updated to allow clearing notes

--- memopad.cpp
+++ memopad.cpp
@@ -15,12 +15,20 @@ int main () {
   string noteBody((istreambuf_iterator<char>(t)),
       istreambuf_iterator<char>());
 
+  cout << "Welcome to memopad" << endl;
   // Open file named cppNotes.txt in root folder of this app
   cppNotes.open("memopad.txt");
 
   // Prompt user for note and save user input to "note" string variable
-  cout << "Enter Note:" << endl;
+  cout << "Enter 'clear' to clear memopad\n\nEnter Note:" << endl;
   getline(cin, note);
+
+
+  if (note == "clear") {
+    noteBody = "";
+    note = "";
+    cout << "memos cleared" << endl;
+  } 
 
   // Create string of previous body followed by new note with line breaks
   cppNotes << noteBody << "\n" << note << "\n";
